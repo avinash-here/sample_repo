6b6a859efd5ba52483d31ab1a93d8e9e0b6f3c13
aivnash-here <avinash.softer@gmail.com>
2022-11-26 12:56:57 +0530

    update it

---
+++ NewProject/src/pack1/Main.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the new environment!");
+		System.out.println("This is Lenovo 13s");
+		
+	}
+
+}
