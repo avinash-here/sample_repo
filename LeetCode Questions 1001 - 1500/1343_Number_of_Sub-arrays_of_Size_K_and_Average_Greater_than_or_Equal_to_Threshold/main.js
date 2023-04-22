4d28576c1e66f668dfeb8d281519d61e0c5baae3
aivnash-here <avinash.softer@gmail.com>
2023-04-22 19:08:18 +0530

    number of sub-arrays

---
+++ LeetCode Questions 1001 - 1500/1343_Number_of_Sub-arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold/main.js
@@ -0,0 +1,38 @@
+/*  // Question   // Level: Medium
+
+Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k 
+and average greater than or equal to threshold.
+ 
+
+Example 1:
+====================================================
+Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+Output: 3
+Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other 
+sub-arrays of size 3 have averages less than 4 (the threshold).
+
+Example 2:
+==============================================================
+Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+Output: 6
+Explanation: The first 6 sub-arrays of size 3 have averages greater than 5. Note that averages are 
+not integers.
+
+*/
+
+// Solution
+
+var numOfSubarrays = function(arr, k, threshold) {
+    let sum = 0, count = 0;
+
+    for(let i = 0; i < k; i++) sum += arr[i];
+    if((sum/k) >= threshold) count++;
+
+    for(let i = k; i < arr.length; i++) {
+        sum += arr[i];
+        sum -= arr[i-k];
+        if((sum/k) >= threshold) count++;
+    }
+
+    return count;    
+};
