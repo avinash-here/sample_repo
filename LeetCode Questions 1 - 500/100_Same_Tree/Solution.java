b960c536d6beff686043f4319f761b5f01badba0
aivnash-here <avinash.softer@gmail.com>
2024-03-10 21:34:47 +0530

    nothing new

--- LeetCode Questions 1 - 500/100_Same_Tree/Solution.java
+++ LeetCode Questions 1 - 500/100_Same_Tree/Solution.java
@@ -2,7 +2,7 @@
 
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.
 
-Two binary trees are considered the same if they are structurally identical, 
+Two binary trees are considered the same if they are structurally identical,
 and the nodes have the same value.
 
 
